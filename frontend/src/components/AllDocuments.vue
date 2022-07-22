df2659f02594fb4a822b9f6293bf1cbd3f750310
Henrique Camargo <henrique.sampaio@madeiramadeira.com.br>
2022-07-22 20:07:07 -0300

    feat: created component list documents

--- frontend/src/components/AllDocuments.vue
+++ frontend/src/components/AllDocuments.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      Projeto iniciado
-    </p>
+    <div id="app">
+      <v-app id="inspire">
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          class="elevation-1"
+        >
+          <template v-slot:[`item.block`]="{ item }">
+            <v-chip
+              :color="getColor(item.block)"
+              dark
+            >
+              {{ item.block }}
+            </v-chip>
+          </template>
+        </v-data-table>
+      </v-app>
+    </div>
   </div>
 </template>
 
@@ -12,6 +27,91 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+  data () {
+    return {
+      headers: [
+        {
+          text: 'id',
+          align: 'start',
+          sortable: false,
+          value: 'id'
+        },
+        { text: 'Número Documento', value: 'documentNumber' },
+        { text: 'Tipo de Documento', value: 'documentType' },
+        { text: 'Bloqueado (g)', value: 'block' }
+      ],
+      desserts: [
+        {
+          id: '2ec22f69-1f4a-46d4-acc8-87d8875ef11a',
+          documentNumber: '159-000-000-11',
+          documentType: 'CPF',
+          block: 'Sim'
+        },
+        {
+          id: '2ec22f69-1f4a-46d4-acc8-87d8875ef11a',
+          documentNumber: '237-000-000-11',
+          documentType: 'CPF',
+          block: 'Sim'
+        },
+        {
+          id: '2ec22f69-1f4a-46d4-acc8-87d8875ef11a',
+          documentNumber: '262-000-000-11',
+          documentType: 'CPF',
+          block: 'Sim'
+        },
+        {
+          id: '2ec22f69-1f4a-46d4-acc8-87d8875ef11a',
+          documentNumber: '305-000-000-11',
+          documentType: 'CPF',
+          block: 'Sim'
+        },
+        {
+          id: '2ec22f69-1f4a-46d4-acc8-87d8875ef11a',
+          documentNumber: '356-000-000-11',
+          documentType: 'CPF',
+          block: 'Sim'
+        },
+        {
+          id: '2ec22f69-1f4a-46d4-acc8-87d8875ef11a',
+          documentNumber: '375-000-000-11',
+          documentType: 'CPF',
+          block: 'Não'
+        },
+        {
+          id: '2ec22f69-1f4a-46d4-acc8-87d8875ef11a',
+          documentNumber: '392-000-000-11',
+          documentType: 'CPF',
+          block: 'Sim'
+        },
+        {
+          id: '2ec22f69-1f4a-46d4-acc8-87d8875ef11a',
+          documentNumber: '408-000-000-11',
+          documentType: 'CPF',
+          block: 'Sim'
+        },
+        {
+          id: '2ec22f69-1f4a-46d4-acc8-87d8875ef11a',
+          documentNumber: '452-000-000-11',
+          documentType: 'CPF',
+          block: 'Não'
+        },
+        {
+          id: '2ec22f69-1f4a-46d4-acc8-87d8875ef11a',
+          documentNumber: '518-000-000-11',
+          documentType: 'CPF',
+          block: 'Sim'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    getColor (block) {
+      if (block === 'Não') return 'green'
+      else return 'red'
+    }
   }
 }
 </script>
