dedea3f523fca454b7ea8d56233a104b24a55d6f
Henrique Camargo <henrique.sampaio@madeiramadeira.com.br>
2022-07-22 00:17:43 -0300

    feat: initial project structure

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Lista de Documentos</router-link> |
+      <router-link to="/about">Criar Documento</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #000000;
+  text-decoration: none
+}
+
+nav a.router-link-exact-active {
+  color: #f6a60c;
+}
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/frajola.png">
+    <AllDocuments msg="Lista de Documentos"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AllDocuments from '@/components/AllDocuments.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    AllDocuments
+  }
+}
+</script>
+
+<style>
+
+.home img {
+    max-width:200px;
+    max-height:150px;
+    width: auto;
+    height: auto;
+}
+
+</style>
